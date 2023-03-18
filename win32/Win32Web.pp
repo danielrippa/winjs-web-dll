afc41291982f75fae801c64b756c4276a2a2016f
danielrippa <621091+danielrippa@users.noreply.github.com>
2023-03-18 19:59:39 -0300

    .

--- win32/Win32Web.pp
+++ win32/Win32Web.pp
@@ -1,1 +1,50 @@
+unit Win32Web;
 
+{$mode delphi}
+
+interface
+
+  type
+
+    TInternetConnection = record
+      ConnectionName: String;
+      IsConnected: Boolean;
+      IsConfigured: Boolean;
+      IsLan: Boolean;
+      IsModem: Boolean;
+      IsOffline: Boolean;
+      IsProxy: Boolean;
+    end;
+
+  function GetInternetConnectionState: TInternetConnection;
+
+implementation
+
+  uses Windows, WinInet;
+
+  function GetInternetConnectionState;
+  var
+    State: DWord;
+    NameLength: Integer;
+    Name: Array[0..128] of Char;
+  begin
+
+    NameLength := SizeOf(Name);
+    FillChar(Name, NameLength, 0);
+
+    with Result do begin
+
+      IsConnected := InternetGetConnectedStateEx(@State, Name, NameLength, 0);
+      ConnectionName := Name;
+
+      IsConfigured := (State and INTERNET_CONNECTION_CONFIGURED) = INTERNET_CONNECTION_CONFIGURED;
+      IsLan        := (State and INTERNET_CONNECTION_LAN)        = INTERNET_CONNECTION_LAN;
+      IsModem      := (State and INTERNET_CONNECTION_MODEM)      = INTERNET_CONNECTION_MODEM;
+      IsOffline    := (State and INTERNET_CONNECTION_OFFLINE)    = INTERNET_CONNECTION_OFFLINE;
+      IsProxy      := (State and INTERNET_CONNECTION_PROXY)      = INTERNET_CONNECTION_PROXY;
+
+    end;
+
+  end;
+
+end.
